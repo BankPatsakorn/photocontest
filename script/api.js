b5b122e1c3ccc1905c800e5ed0641243ec2d4309
Patsakorn Rittitum <bank.patsakorn@gmail.com>
2021-11-21 10:03:30 +0700

    add loading

--- script/api.js
+++ script/api.js
@@ -3,44 +3,41 @@
 // });
 
 var data = '[{"id": 1,"funcCampaign": "PhotoContest","userId": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","lineId": "Ue543604560f7aae69251d07f6ebefe4e","isFinal": false,"urlImage": "photocontest.png","urlImageSmall": "photocontest.png","submitDate": "2021-11-20T14:41:20.973Z","submitFrom": "line","createBy": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","createDate": "2021-11-20T14:41:28.020Z","updateBy": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","updateDate": "2021-11-20T14:41:28.020Z"},{"id": 2,"funcCampaign": "PhotoContest","userId": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","lineId": "Ue543604560f7aae69251d07f6ebefe4e","isFinal": false,"urlImage": "photocontest.png","urlImageSmall": "photocontest.png","submitDate": "2021-11-20T15:25:11.373Z","submitFrom": "line","createBy": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","createDate": "2021-11-20T15:25:19.397Z","updateBy": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","updateDate": "2021-11-20T15:25:19.397Z"},{"id": 3,"funcCampaign": "PhotoContest","userId": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","lineId": "Ue543604560f7aae69251d07f6ebefe4e","isFinal": false,"urlImage": "photocontest.png","urlImageSmall": "photocontest.png","submitDate": "2021-11-20T15:25:22.343Z","submitFrom": "line","createBy": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","createDate": "2021-11-20T15:25:24.077Z","updateBy": "523E302E-1F1F-4EA9-A982-27DC9A208FAF","updateDate": "2021-11-20T15:25:24.077Z"}]';
-
+var listImages;
 
 function getUserImages(objJsonData) {
-  var obj = JSON.parse(data);
-  console.log('dd'+obj[0].urlImageSmall);
-  console.log('dd'+obj.length);
-  document.getElementById("numpic").innerHTML = "("+obj.length+"/20)";
 
-  var stringHtml = "";
-  obj.forEach(element => {
-    console.log(element.urlImageSmall);
-    stringHtml +=  "<div>";
-    stringHtml +=  "<img src='https://dev.toyotaprofiling.com/photocontest2021/result_AW/"+ element.urlImageSmall +"' height='60'>";
-    stringHtml +=  "</div>";
+  $.ajax({
+    url: "https://dev.toyotaprofiling.com/api/photocontest-result/getimage",
+    method: "POST",
+    data: objJsonData,
+    dataType: 'json',
+    contentType: "application/json",
+    success: function (result) {
+      console.log('success : ' + result);
+      listImages = JSON.parse(data);
+      document.getElementById("numpic").innerHTML = "("+listImages.length+"/20)";
+    
+      var stringHtml = "";
+      listImages.forEach(element => {
+        stringHtml +=  "<div>";
+        stringHtml +=  "<img src='https://dev.toyotaprofiling.com/photocontest2021/result_AW/"+ element.urlImageSmall +"' height='60'>";
+        stringHtml +=  "</div>";
+      });
+    
+      for(let i=0; i<20-listImages.length; i++){
+        stringHtml +=  "<div>";
+        stringHtml +=  "<img src='http://placehold.it/300x100' height='60'>";
+        stringHtml +=  "</div>";
+      }
+      var section = document.getElementById("sectionPictures");
+      section.innerHTML = stringHtml;
+
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      console.log('Error:' + errorThrown);
+    }
   });
-
-  for(let i=0; i<20-obj.length; i++){
-    stringHtml +=  "<div>";
-    stringHtml +=  "<img src='http://placehold.it/300x100' height='60'>";
-    stringHtml +=  "</div>";
-  }
-  console.log(stringHtml);
-  var section = document.getElementById("sectionPictures");
-  section.innerHTML = stringHtml;
-  // $.ajax({
-  //   url: "https://dev.toyotaprofiling.com/api/photocontest-result/getimage",
-  //   method: "POST",
-  //   data: objJsonData,
-  //   dataType: 'json',
-  //   contentType: "application/json",
-  //   success: function (result) {
-  //     console.log('success : ' + result);
-
-  //   },
-  //   error: function (XMLHttpRequest, textStatus, errorThrown) {
-  //     console.log('Error:' + errorThrown);
-  //   }
-  // });
 }
 function sendData(objJsonData,jsonImgBase64) {
   console.log(jsonImgBase64);
