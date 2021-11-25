d791f131ed32f7b51dbd4e998103a1ea38b14468
pollawat <pollawatstp@gmail.com>
2021-11-25 22:26:36 +0700

    Resize > 3 mb

--- script/photocontest.js
+++ script/photocontest.js
@@ -1,5 +1,6 @@
 var base64image = "";
 var base64imageSmall = "";
+var base64imageLg = "";
 const urlParams = new URLSearchParams(window.location.search);
 const lineId = urlParams.get('lineId');//"Ue543604560f7aae69251d07f6ebefe4e";
 const platform = urlParams.get('platform');//"Line";
@@ -21,9 +22,9 @@ $(document).ready(function () {
 
 });
 function submit() {
-    if(document.getElementById('OutputImg').getAttribute('src') === 'img/aw/empty_pic.jpg'){
+    if (document.getElementById('OutputImg').getAttribute('src') === 'img/aw/empty_pic.jpg') {
         alertUploadPic();
-    }else{
+    } else {
         if (listImages.length == 20) {
             alertPicLimit();
         } else {
@@ -31,63 +32,107 @@ function submit() {
             var filepath = lineId + "_" + listImages.length;
             var filepathsmall = lineId + "_sm_" + listImages.length;
             var currentData = new Date();
-    
-            var obj = { lineId: lineId, urlImage: filepath + "." + fileType, urlImageSmall: filepathsmall + ".png", submitDate: currentData, submitFrom: (platform ? platform : 'Line') };
+
+            var obj = { lineId: lineId, urlImage: filepath + "." + fileType, urlImageSmall: filepathsmall + ".jpeg", submitDate: currentData, submitFrom: (platform ? platform : 'Line') };
             var objJsonData = JSON.stringify(obj);
-    
-    
+
+
             var objImage = { data: base64image, picname: filepath };
             var objJsonImage = JSON.stringify(objImage);
-    
+
             var objImageSmall = { data: base64imageSmall, picname: filepathsmall };
-            var objJsonImageSmall  = JSON.stringify(objImageSmall);
-    
-            sendData(objJsonData, objJsonImage ,objJsonImageSmall );
-           
+            var objJsonImageSmall = JSON.stringify(objImageSmall);
+
+            sendData(objJsonData, objJsonImage, objJsonImageSmall);
+
         }
     }
-    
+
 }
 function encodeImageFileAsURL(event) {
 
-    var output = document.getElementById('resize');
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
+    let output = document.getElementById('resize');
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+
 
     canvas.width = 128; // target width
     canvas.height = 128; // target height
 
-
-    var file = event.target.files[0];
+    console.log(event);
+    let file = event.target.files[0];
     fileType = event.target.files[0].name.split('.').pop().toLowerCase();
-    if(fileType == "jpg"){
+    if (fileType == "jpg") {
         fileType = "jpeg";
     }
 
-    
+
     console.log("fileType : " + fileType);
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.onloadend = function () {
-        console.log('RESULT', reader.result )
+        console.log('RESULT', reader.result)
+        //console.log('width', reader.width)
         base64image = reader.result;
 
-        var srcData = reader.result; // <--- data: base64
-        var newImage = document.createElement('img');
+        let srcData = reader.result; // <--- data: base64
+        let newImage = document.createElement('img');
+        let img = document.createElement('img');
         newImage.src = srcData;
+        img.src = srcData;
+        //Resize Small
+        newImage.onload = function (e) {
 
-        //Resize
-        newImage.onload = function(e) {
-     
             ctx.drawImage(newImage, 0, 0, canvas.width, canvas.height);
             // create a new base64 encoding       
-            console.log('RESULT Resize', canvas.toDataURL())
-            output.src = canvas.toDataURL();
-            base64imageSmall = canvas.toDataURL();
+            console.log('RESULT Resize', canvas.toDataURL("image/jpeg"))
+            output.src = canvas.toDataURL("image/jpeg");
+            base64imageSmall = canvas.toDataURL("image/jpeg");
         };
+
+
 
     }
     reader.readAsDataURL(file);
 }
+
+
+function encodeImageFileAsURL2(event) {
+
+    let output = document.getElementById('resize2');
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+
+    console.log(event);
+    let file = event.target.files[0];
+
+    let reader = new FileReader();
+    reader.onloadend = function () {
+
+        base64image = reader.result;
+
+        let srcData = reader.result; // <--- data: base64
+        let newImage = document.createElement('img');
+        let img = document.createElement('img');
+        newImage.src = srcData;
+        img.src = srcData;
+        //Resize Lg
+        newImage.onload = function (e) {
+            canvas.width = newImage.width * 0.5;
+            canvas.height = newImage.height * 0.5;
+            //   
+            ctx.drawImage(newImage, 0, 0, canvas.width, canvas.height);
+            // create a new base64 encoding       
+            console.log('RESULT Resizelg', canvas.toDataURL("image/jpeg"))
+            output.src = canvas.toDataURL("image/jpeg");
+            base64image = canvas.toDataURL("image/jpeg");
+            fileType = "jpeg";
+        };
+    }
+    reader.readAsDataURL(file);
+}
+
+
+
 function changePage(i) {
     var divPage = document.getElementById(i);
     divPage.style.display = 'none';
@@ -97,7 +142,6 @@ function changePage(i) {
 var loadFile = function (event) {
     validateSize(event);
     encodeImageFileAsURL(event);
-    console.log(event);
     var output = document.getElementById('OutputImg');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function () {
@@ -109,8 +153,9 @@ var loadFile = function (event) {
 };
 function validateSize(input) {
     const fileSize = input.target.files[0].size / 1024 / 1024; // in MiB
+    //fileSize > 5 ให้ย่อรูปลง /2
     if (fileSize > 3) {
-
+        encodeImageFileAsURL2(input);
         alert('File size exceeds 2 MiB');
         // $(file).val(''); //for clearing with Jquery
     } else {
